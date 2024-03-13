3f35f4989967a0c9c88142ca90cdf1121d3fb3ea
Rodrigo Veloso <57206601+rodrigov5@users.noreply.github.com>
2024-03-12 23:29:24 -0300

    :wrench: esting route system :wrench:

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,35 @@ namespace core;
 
 class Router
 {
+    private $controller = 'Site';
+    private $method = 'home';
+    private $param = [];
+
     public function __construct()
     {
-        echo 'Hello from Router!';
+        $router = $this->url();
+
+        if (file_exists('app/controllers/' . ucfirst($router[0]) . '.php')) {
+            $this->controller = $router[0];
+            unset($router[0]);
+
+            $class = "\\app\\controllers\\" . ucfirst($this->controller);
+            $object = new $class;
+
+            if (isset($router[1]) && method_exists($class, $router[1])) {
+                $this->method = $router[1];
+                unset($router[1]);
+            }
+
+            $this->param = $router ? array_values($router) : [];
+
+            call_user_func_array([$object, $this->method], $this->param);
+        }
+    }
+
+    private function url()
+    {
+        $parse_url = explode("/", filter_input(INPUT_GET, 'router', FILTER_SANITIZE_URL));
+        return $parse_url;
     }
 }

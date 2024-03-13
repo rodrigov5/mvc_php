327b1f9383498ff191986b8350bfd9372809f06c
Rodrigo Veloso <rodrigovelosow@gmail.com>
2024-03-13 11:45:36 -0300

    :star:

---
+++ app/controllers/Site.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+class Site
+{
+    public function home()
+    {
+        echo "Hello from home method in Site controller";
+    }
+}
--- config/config.php
+++ config/config.php
@@ -6,31 +6,10 @@
     <title>Mvc PHP</title>
 
     <link rel="stylesheet" href="/config/materialize/css/materialize.min.css">
-
-    <style>
-        .nav-wrapper {
-            padding-left: 5rem; /* Adjust the left padding as needed */
-            padding-right: 5rem; /* Adjust the right padding as needed */
-        }
-    </style>
 </head>
 <body>
     
-    <body>
-    <nav class="teal lighten-2">
-    <div class="nav-wrapper">
-        <a href="#" class="brand-logo">Logo</a>
-        <a href="#" data-target="mobile-nav" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-        <ul class="right hide-on-med-and-down">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Products</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
-    </div>
-</nav>
-    </body>
+    <nav class="teal lighten-2"></nav>
 
     <script src="/config/materialize/js/materialize.min.js"></script>
 </body>
